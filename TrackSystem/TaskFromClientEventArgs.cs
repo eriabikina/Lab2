5c4ee013621d8d9bc2997cb4fbd6f3b75c01d847
era <eriabikina@gmail.com>
2017-06-30 17:55:35 +0200

    Update #15 for Lab2

--- TrackSystem/TaskFromClientEventArgs.cs
+++ TrackSystem/TaskFromClientEventArgs.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TrackSystem {
-  public  class TaskFromClientEventArgs : EventArgs {
-       
+  public  class TaskFromClientEventArgs : EventArgs {     
 
         
     }
